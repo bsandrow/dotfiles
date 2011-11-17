3f6092b3b45819f3b123ca6fd5e81b00bea224f4
Brandon Sandrowicz <brandon@sandrowicz.org>
2011-11-17 00:43:45 -0800

    Converting zsh setup: Python settings

---
+++ python/shell.bash
@@ -0,0 +1,16 @@
+### Python Shell
+#
+#  Launch an interactive Python shell.
+#
+function python_shell()
+{
+    [ -x "$PYSHELL" ] || PYSHELL=$(which ipython)
+    [ -x "$PYSHELL" ] || PYSHELL=$(which python)
+    [ -x "$PYSHELL" ] || { echo "Could not find a Python interpreter!" >&2; return 1; }
+
+    $PYSHELL
+}
+
+alias pysh="python_shell"
+
+# vim: set filetype=sh:
