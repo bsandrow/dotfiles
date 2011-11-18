ddf65f6c638a4a612f3e7508418b778073588b9a
Brandon Sandrowicz <brandon@sandrowicz.org>
2011-11-18 09:32:57 -0800

    Adding perl shell setup

---
+++ perl/env.bash
@@ -0,0 +1,9 @@
+
+#--------------------------------------------------------------------------------
+# Perlbrew Environment
+#--------------------------------------------------------------------------------
+#  Source the perlbrew setup, if it exists.
+#
+if [ -f "$HOME/perl5/perlbre/etc/bashrc" ]; then
+    source "$HOME/perl5/perlbrew/etc/bashrc"
+fi
