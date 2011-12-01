df778ea135ab1c4384753096fa708ccb9e884707
Brandon Sandrowicz <brandon@sandrowicz.org>
2011-11-30 19:05:03 -0800

    Fixing a typo in Perl shell env files

--- perl/env.bash
+++ perl/env.bash
@@ -4,6 +4,6 @@
 #--------------------------------------------------------------------------------
 #  Source the perlbrew setup, if it exists.
 #
-if [ -f "$HOME/perl5/perlbre/etc/bashrc" ]; then
+if [ -f "$HOME/perl5/perlbrew/etc/bashrc" ]; then
     source "$HOME/perl5/perlbrew/etc/bashrc"
 fi
