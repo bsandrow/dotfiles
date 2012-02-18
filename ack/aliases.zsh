db17fde89fecb298a7bbe63eae73d82fba1702fc
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-02-17 16:54:22 -0800

    Streamlining ack shell setup

--- ack/aliases.zsh
+++ ack/aliases.zsh
@@ -1,22 +1,22 @@
+#----------------------------------------
+# Ack Grep
+#----------------------------------------
 
-
-###########
-# Find Ack
-#
-function find_ack() {
-    # On Ubuntu/Debian the ack command is ack-grep in the repos, but we might also have installed
-    # 'ack' from source into ~/bin. Check if 'ack' is the command we think it is, otherwise we use
-    # ack-grep.
-    if lsb_release --id | grep -iq 'ubuntu\|debian'; then
-        if [ -x "$(which ack 2> /dev/null)" ] && ack --version | grep -q 'Andy Lester'; then
-            echo "ack"
-        else
-            echo "ack-grep"
-        fi
-    else
-        echo "ack"
-    fi
+function alias_ack()
+{
+    release_id=$(lsb_release --id | cut -d '	' -f 2)
+    case $release_id in
+        Ubuntu|Debian)
+            ack_cmd="$(which ack 2> /dev/null)"
+            if [ -x "$ack_cmd" ] && ack --version | grep -q 'Andy Lester'; then
+                alias ack="ack"
+            else
+                alias ack="ack-grep"
+            fi
+            ;;
+        *)
+            alias ack="ack"
+            ;;
+    esac
 }
-alias ack='$(find_ack)'
-
-# vim: set ft=sh et ts=4 sw=4 tw=100:
+alias_ack
