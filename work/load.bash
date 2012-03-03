57ecd21085a08af665d679e1a318345e77263865
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-03-02 19:49:48 -0800

    Adding auto-loading for work shell setup

---
+++ work/load.bash
@@ -0,0 +1,8 @@
+#----------------------------------------
+# Work Settings
+#----------------------------------------
+
+WORK_BASHRC="$WORKDIR/conf/dotfiles/bash/bashrc"
+if [ -f "$WORK_BASHRC" ]; then
+    source "$WORK_BASHRC"
+fi
---
+++ work/load.zsh
@@ -0,0 +1,7 @@
+#----------------------------------------
+# Work Settings
+#----------------------------------------
+WORK_ZSHRC="$WORKDIR/conf/dotfiles/zsh/zshrc"
+if [ -f "$WORK_ZSHRC" ]; then
+    source $WORK_ZSHRC
+fi
