4fed228196b6e4dac7508de4d04bea8ac5bb5073
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-03-03 01:29:42 -0800

    Fixing virtualenv shell setup
    If a virtualenv was already setup, then virtualenvwrapper would use that
    Python as the base python, which doesn't work very well.

--- python/shell.zsh
+++ python/shell.zsh
@@ -19,6 +19,11 @@ alias pysh="python_shell"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export WORKON_HOME="$HOME/.virtualenv"
 
+# If a virtualenv is already enabled, then this variable will be screwed up
+# (using a python instance within a virtualenv is a no-no). We need to ensure
+# that this is correct.
+export VIRTUALENVWRAPPER_PYTHON=$(which -a python | grep -v "$WORKON_HOME" | head -1)
+
 if [ $(lsb_release --id | cut -d '	' -f 2) = "Ubuntu" ]; then
     ubuntu_virtualenvwrapper="/etc/bash_completion.d/virtualenvwrapper"
     if [ -f "$ubuntu_virtualenvwrapper" ]; then
