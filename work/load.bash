3152bb04244570da2d3ca686e3384ee332378c9e
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-04-07 15:39:21 -0400

    Removing work shell config
    The work shell config loading can either be
    the .bashrc.local/.zshrc.local or be loaded
    in there.

--- work/load.bash
+++
@@ -1,8 +0,0 @@
-#----------------------------------------
-# Work Settings
-#----------------------------------------
-
-WORK_BASHRC="$WORKDIR/conf/dotfiles/bash/bashrc"
-if [ -f "$WORK_BASHRC" ]; then
-    source "$WORK_BASHRC"
-fi
--- work/load.zsh
+++
@@ -1,7 +0,0 @@
-#----------------------------------------
-# Work Settings
-#----------------------------------------
-WORK_ZSHRC="$WORKDIR/conf/dotfiles/zsh/zshrc"
-if [ -f "$WORK_ZSHRC" ]; then
-    source $WORK_ZSHRC
-fi
