e90a189ca6dfce8a2a604a96105c5a326af9e9d8
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-04-10 17:44:23 -0400

    Converting grabssh/fixssh setup to use $GRABSSH_DIR variable

--- ssh/shell.bash
+++ ssh/shell.bash
@@ -6,9 +6,10 @@
 # ======
 #   This is paired with the grabssh script to allow refreshing of SSH
 #   environment variables on stale remote shell sessions (i.e. detach screen
-#   session an reattach from a new ssh connection).
+#   session an reattach from a new ssh connection). Note: $GRABSSH_DIR must be
+#   set.
 #
-alias fixssh='if [ -f "${HOME}/tmp/fixssh" ]; then source "${HOME}/tmp/fixssh"; else echo "No $HOME/tmp/fixssh found!"; fi'
+alias fixssh='if [ -f "${GRABSSH_DIR}/fixssh" ]; then source "${GRABSSH_DIR}/fixssh"; else echo "No ${GRABSSH_DIR}/fixssh found!"; fi'
 
 # Tab-Completion
 # ==============
