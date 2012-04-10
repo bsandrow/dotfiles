b73da37f1e401249670eac62e6867e704ff1276c
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-04-10 17:39:57 -0400

    Adding bash-specific ssh config

--- ssh/shell.bash
+++ ssh/shell.bash
@@ -1,10 +1,15 @@
-# Fix ssh environment variables in remote screen sessions. The 'fixssh' file is
-# created by another script which exports all of the environment variables that
-# need to be updated in the screen session.
+#-------------------------------------------------------------------------------
+# SSH
+#-------------------------------------------------------------------------------
+
+# fixssh
+# ======
+#   This is paired with the grabssh script to allow refreshing of SSH
+#   environment variables on stale remote shell sessions (i.e. detach screen
+#   session an reattach from a new ssh connection).
 #
-# [ When you detach a remote screen session and reattach from a new ssh
-# connection, some of the environment vars used for things like ssh-agent
-# forwarding are not updated. These need to be exported to a eval'able file
-# from the new login shell, before ssh is attached. Then all shells in the ssh
-# session can just run 'fixssh' to import the updated env var values. ]
 alias fixssh='if [ -f "${HOME}/tmp/fixssh" ]; then source "${HOME}/tmp/fixssh"; else echo "No $HOME/tmp/fixssh found!"; fi'
+
+# Tab-Completion
+# ==============
+source "$_/ssh-config-completion.bash"
