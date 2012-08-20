8f6c23f0496f48d4218d1fc2076bd46af0232e26
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-08-20 00:51:40 -0400

    Adding GNOME Terminal auto-detection
    If a shell is started in a GNOME Terminal, then we set the TERM to
    xterm-256color (seeing as GNOME Terminal has fully supported 256 colors
    for a while now -- and there is no ability to force GNOME Terminal to
    set this itself).

--- zsh/gnome-terminal.zsh
+++ zsh/gnome-terminal.zsh
@@ -1,3 +1,6 @@
+# GNOME Terminal
+# ~~~~~~~~~~~~~~
+
 function running_in_gnome_terminal()
 {
     # Note: This only works if the shell is a direct child process of a
@@ -7,16 +10,21 @@ function running_in_gnome_terminal()
     #       because there is no easy facility for this in GNOME Terminal's preferences.
 
     parent_process=$PPID
-    ps -e | while read line; do
+    ps -e | grep 'gnome-terminal' | while read line; do
         clean_line=$(echo "$line" | sed -e 's/^[[:space:]]\+//' -e 's/[[:space:]]\+/ /g')
 
         process_id=$(echo "$clean_line" | cut -d' ' -f1)
-        if [ "$process_id" != "$parent_process" ]; then
-            continue
-        fi
+        if [ "$process_id" = "$parent_process" ]; then
 
-        process_name=$(echo "$clean_line" | cut -d' ' -f4)
-        return [ "$process_name" = "gnome-terminal" ]
+            process_name=$(echo "$clean_line" | cut -d' ' -f4)
+            if [ "$process_name" = "gnome-terminal" ]; then
+                return 0
+            fi
+        fi
     done
     return 1
 }
+
+if [ "$TERM" = "xterm" ] && running_in_gnome_terminal; then
+    export TERM=xterm-256color
+fi
